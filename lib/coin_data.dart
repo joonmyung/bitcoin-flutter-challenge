aa8ac81af1be731db94be07a01addc8bf09764d4
Joonmyung <mytwiny@gmail.com>
2019-08-14 23:11:47 +0900

    Level 1 - Bronze Medal challenge

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,4 +1,8 @@
 //TODO: Add your imports here.
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 const List<String> currenciesList = [
   'AUD',
@@ -35,4 +39,27 @@ const bitcoinAverageURL =
 
 class CoinData {
   //TODO: Create your getCoinData() method here.
+
+  String requestURL = '$bitcoinAverageURL/BTCUSD';
+
+  Future getCoinData() async {
+    http.Response response = await http.get(
+        Uri.encodeFull(requestURL),
+        headers: {
+          "Accept": "application/json"
+        }
+    );
+
+    if (response.statusCode == 200 ) {
+      var decodeData = jsonDecode(response.body);
+      var lastPrice = decodeData['last'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
+    }
+
+
+  }
+
 }
